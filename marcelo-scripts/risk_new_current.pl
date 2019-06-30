b17d1f1012d48b3a894dae2b08c9ee6faed21762
Your Name <you@example.com>
2019-06-30 19:13:17 +0300

    sdfsdf

--- marcelo-scripts/risk_new_current.pl
+++ marcelo-scripts/risk_new_current.pl
@@ -34,7 +34,7 @@ my $term = Term::ReadLine->new('Foobar');
 while (1){
  {
   no warnings "uninitialized";
-  ($pos_portfolio,$sect_price,$stop_price,$buy_or_sell) = undef;
+  ($pos_portfolio,$sect_price,$stop_price,$tget_price,$buy_or_sell) = undef;
   $cash = $term->readline('Enter cash amount: ') until looks_like_number($cash) and $cash > 0;
   $percnt_cur = $term->readline('Enter current working risk percent: ') until looks_like_number($percnt_cur) and $percnt_cur > 0;
   $sect_price = $term->readline('Enter security price: ') until looks_like_number($sect_price) and $sect_price > 0;
@@ -52,12 +52,16 @@ while (1){
  }
 
 
- $risk_ratio = $raw_risk_percnt / $percnt_cur;
- $real_profit_percnt = $raw_profit_percnt / $risk_ratio;
+if ($raw_risk_percnt <= $percnt_cur ){
+	$risk_ratio = 1;
+}else{
+ 	$risk_ratio = $raw_risk_percnt / $percnt_cur;
+}
+$real_profit_percnt = $raw_profit_percnt / $risk_ratio;
 
  $risk_cash = $cash / $risk_ratio;
  $pos_orderentry = int($risk_cash/$sect_price);
 
  say "Going ", do {"long " if ($buy_or_sell eq 'buy')}, do {"short " if ($buy_or_sell eq 'sell')}, ":\n","Raw risk percent is ", $raw_risk_percnt, "\n","Raw profit percent is ", $raw_profit_percnt, "\n",
- 	"Real profit percent is ", $real_profit_percnt, "\n", "Position size is ", $pos_orderentry, "\n", "Risking $risk_cash in cash", "\n\n", "A 1 to '",$raw_profit_percnt/$raw_risk_percnt, "' risk to win ratio";
+ 	"Real profit percent is ", $real_profit_percnt, "\n", "Position size is ", $pos_orderentry, "\n", "Risking $risk_cash in cash", "\n\n", "A 1 to '",$raw_profit_percnt/$raw_risk_percnt, "' risk to win ratio\n","Potential Loss in cash ", $raw_risk_percnt/100 * $risk_cash,"\n", "Potential win in cash ", $raw_profit_percnt/100 * $risk_cash;
 }
