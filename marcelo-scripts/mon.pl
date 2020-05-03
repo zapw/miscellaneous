4c687c2bc19b0532a596db1d72aad90a4913ae96
Marcelo Eskinasi Azevedo <zapw@users.noreply.github.com>
2020-05-03 18:57:07 +0300

    Add files via upload

--- marcelo-scripts/mon.pl
+++ marcelo-scripts/mon.pl
@@ -5,19 +5,28 @@
 my $cash = 40; 
 
 
-my $times = 2*12;
+my $times = 144;
 my $years = 10;
+my $year_win;
+my $win;
 
 for (my $x=1;$x<=$years;$x++){
- print "Year $x\n";
- my $year_win = 0;
+ print "Year $x\n\n";
+ $year_win = 0;
  for (my $i=1;$i<=$times;$i++){
+    $win = 0;
     my $trade = $cash/7;
     $trade = 60 if $trade > 60; 
-    my $win = $trade*0.76;
+    if ($i % 5 == 0 ){
+	$win = $trade*0.76;
+	print 'Win ';
+    }else{
+	$win = $trade*-0.07;
+	print 'Loss ';
+    }
     $cash += $win;
     $year_win += $win; 
-    print "Win $win, total cash after win $cash, cash in position $trade\n";
+    print "$win, total cash after win $cash, cash in position $trade\n";
  }
  $cash -= $year_win*0.25;
  $cash -= 51.42;
