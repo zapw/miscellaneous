0d5734d5644948479365b610473f7b050b6ec682
Marcelo Eskinasi Azevedo <zapw@users.noreply.github.com>
2020-09-22 09:57:42 +0300

    Add files via upload

--- marcelo-scripts/fast_risk.pl
+++ marcelo-scripts/fast_risk.pl
@@ -26,10 +26,10 @@ use bignum;
 
 
 my $risk_ratio = 1;
-my $risk_percent = 5;
+my $risk_percent = 10;
 my $risk_percent_weak = $risk_percent/2; 
 my $cash_multiplier = 1000;
-my $cash_divider = 7; # 1/7 of total cash per position
+my $cash_divider = 10; # 1/10 of total cash per position
 my $half = 0;
 my (@cash,$entry,$stop);
 
@@ -41,7 +41,7 @@ sub exit_fun {
 exit_fun if @ARGV <= 1;
 
 
-GetOptions('cash=f{1,}' => \@cash, 'entry=f{1,1}' => \$entry, 'stop=f{1,1}' => \$stop, 'half' => \$half);
+GetOptions('cash=f{1,}' => \@cash, 'entry=f{1,1}' => \$entry, 'stop:f{1,1}' => \$stop, 'half' => \$half);
 
 $risk_percent = $risk_percent_weak if $half == 1;
 my $pos_orderentry;
@@ -54,26 +54,27 @@ $total *= $cash_multiplier;
 
 $cash_per_trade = $total / $cash_divider;
 
+unless ($stop == 0){
+	if ($entry < $stop){
+		$percent_calculated = (abs($stop/$entry) - 1) * 100;
+	}elsif($entry > $stop){
+		$percent_calculated = (1 - abs($stop/$entry)) * 100;
+	}
 
-if ($entry < $stop){
-	$percent_calculated = (abs($stop/$entry) - 1) * 100;
-}elsif($entry > $stop){
-	$percent_calculated = (1 - abs($stop/$entry)) * 100;
+
+	#risk only 10 percent of $total money don't put more money because stop loss is less than $risk_percent
+	#if ($percent_calculated < $risk_percent){ 
+	#	$risk_ratio = 1 / $risk_percent / $percent_calculated;
+	#}elsif ($percent_calculated > $risk_percent){
+	if ($percent_calculated > $risk_percent){
+			$risk_ratio = $percent_calculated / $risk_percent;
+			say "Warning $percent_calculated"."% is larger than $risk_percent"."%, risk divider ($risk_ratio)";
+	}
+
+	#say " percent calculated is $percent_calculated";
+	#say " risk percent is $risk_percent";
+	#say " risk_ratio is $risk_ratio";
 }
-
-
-#risk only 10 percent of $total money don't put more money because stop loss is less than $risk_percent
-#if ($percent_calculated < $risk_percent){ 
-#	$risk_ratio = 1 / $risk_percent / $percent_calculated;
-#}elsif ($percent_calculated > $risk_percent){
-if ($percent_calculated > $risk_percent){
-		$risk_ratio = $percent_calculated / $risk_percent;
-		say "Warning $percent_calculated"."% is larger than $risk_percent"."%, risk divider ($risk_ratio)";
-}
-
-#say " percent calculated is $percent_calculated";
-#say " risk percent is $risk_percent";
-#say " risk_ratio is $risk_ratio";
 $risk_cash = $cash_per_trade / $risk_ratio;
 
 #don't care about actual putting half of 1/10 of money, care more about putting half of the usual risk on a trade
